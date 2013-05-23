ebf16a99312235b75e849a40210a6fb469fcd926
fsg <felixgallo@gmail.com>
2013-05-23 12:50:49 -0700

    fixes

--- apps/skel/src/udp_server.erl
+++ apps/skel/src/udp_server.erl
@@ -28,10 +28,13 @@ handle_info(stats, State) ->
         io:format("~p: ~p~n", [{Host, Port}, ConnState])
     end, infonotused, udp_clients),
   {noreply, State};
+handle_info({makespam, Ip}, State) ->
+  spam(10000, Ip, 9595, 1),
+    {noreply, State};
 handle_info(makespam, State) ->
   io:format("spamming~n"),
-  spam(10000, {127,0,0,1}, 9595, 1),
-    {noreply, State};
+  spam(10000, {166,78,12,187}, 9595, 1),
+  {noreply, State};
 handle_info(Info, State) ->
     io:format("got message: ~p~n", [Info]),
     {noreply, State}.
@@ -46,6 +49,7 @@ server(Port) ->
   {ok, Socket} = gen_udp:open(Port, [binary, {active, false}, {recbuf, 65536}, {sndbuf, 65536}, {buffer, 65536}, {read_packets, 16000}]),
   io:format("server opened socket:~p~n",[Socket]),
   ets:new(udp_clients, [set, named_table]),
+  ets:new(pregistry, [set, named_table, public]),
   %Heartbeat = spawn(fun() -> heartbeat(Socket) end),
   %link(Heartbeat),
   loop(Socket).
@@ -57,13 +61,9 @@ loop(Socket) ->
       case ets:lookup(udp_clients, { Host, Port }) of
         [] -> 
           % new client
-          %io:format("never seen ~p before~n", [{ Host, Port } ]),
-          ets:insert(udp_clients, { { Host, Port }, 1 }),
+          ets:insert(udp_clients, { { Host, Port }, 0 }),
           handle(Socket, Host, Port, {1}, Bin);
         [{{Host, Port}, ConnState}] ->
-          % previously seen client
-          ets:update_counter(udp_clients, { Host, Port }, 1),
-          %io:format("oh hey return visitor ~p~n", [{Host, Port, ConnState}]),
           handle(Socket, Host, Port, ConnState, Bin);
         [_Unknown] -> 
           io:format("bad news!~n")
@@ -83,10 +83,17 @@ heartbeat(Socket) ->
   heartbeat(Socket).
 
   
-handle(Socket, Host, Port, ConnState, <<Command/utf8, Id:16, Seq:16, Ack:16, Appstring/binary>>) ->
-  Outbound = <<65:8, Id:16, Seq:16, Ack:16, Appstring/binary>>,
+handle(Socket, Host, Port, ConnState, <<Command/utf8, Id:32, Seq:16, Ack:16, Appstring/binary>>) ->
   %io:format("handling: ~p~n", [{Host, Port, ConnState, Command, Id, Seq, Ack, Appstring}]),
-  gen_udp:send(Socket, Host, Port, Outbound);
+  ets:update_counter(udp_clients, { Host, Port }, 1),
+  ets:update_counter(pregistry, Id, 1),
+  case Command of
+    65 ->
+      ok;
+    _ -> 
+    Outbound = <<65:8, Id:32, Seq:16, Ack:16, Appstring/binary>>,
+    gen_udp:send(Socket, Host, Port, Outbound)
+  end;
 handle(_, _, _, _, _) -> err.
 
 spam(Amount, Host, Port, PacketLength) ->
@@ -96,8 +103,13 @@ spam(Amount, Host, Port, PacketLength) ->
 
 spam_impl(0, _, _, _, _) -> ok;
 spam_impl(Amount, Socket, Host, Port, PacketLength) ->
+  Seq = 0,
+  Ack = 0,
+  Appstring = <<"boop">>,
+  Outbound = <<99:8, Amount:32, Seq:16, Ack:16, Appstring/binary>>,
   timer:sleep(1),
   erlang:yield(),
-  gen_udp:send(Socket, Host, Port, <<65:8>>),
+  gen_udp:send(Socket, Host, Port, Outbound),
+  ets:insert(pregistry, { Amount, 0 }),
   spam_impl(Amount -1, Socket, Host, Port, PacketLength).
   
